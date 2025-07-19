9bf225c731cb761b0acad145bb46a799829d562f
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-07-19 10:32:52 +0200

    fixed imports

--- app/src/main/java/com/example/itsolutionspp/config/SecondaryDbConfig.java
+++ app/src/main/java/com/example/itsolutionspp/config/SecondaryDbConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
