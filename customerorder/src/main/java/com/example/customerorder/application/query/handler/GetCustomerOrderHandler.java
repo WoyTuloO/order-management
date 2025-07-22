a8004901d867da03358fbc7e9564c79387b3e53e
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-07-22 12:47:19 +0200

    added get handler for CustomerOrder

--- customerorder/src/main/java/com/example/customerorder/application/query/handler/GetCustomerOrderHandler.java
+++ customerorder/src/main/java/com/example/customerorder/application/query/handler/GetCustomerOrderHandler.java
@@ -1,13 +1,19 @@
 package com.example.customerorder.application.query.handler;
 
 
+import com.example.customerorder.adapter.dto.response.CustomerOrderItemResponse;
 import com.example.customerorder.application.port.in.GetCustomerOrderUseCase;
 import com.example.customerorder.application.port.out.CustomerOrderRepositoryPort;
 import com.example.customerorder.application.query.GetCustomerOrderQuery;
-import com.example.customerorder.application.query.GetCustomerOrderResponse;
+import com.example.customerorder.adapter.dto.response.GetCustomerOrderResponse;
 import com.example.customerorder.domain.model.aggregate.CustomerOrder;
+import com.example.manufacturingorder.adapter.in.rest.dto.response.GetManufacturingOrderResponse;
+import com.example.manufacturingorder.application.query.getCustomersManufacturingOrders.GetCustomersManufacturingOrdersQuery;
+import com.example.manufacturingorder.application.query.getCustomersManufacturingOrders.handler.GetCustomersManufacturingOrdersHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,13 +21,18 @@ public class GetCustomerOrderHandler implements GetCustomerOrderUseCase {
 
     private final CustomerOrderRepositoryPort customerOrderRepositoryPort;
 
+    private final GetCustomersManufacturingOrdersHandler getCustomersManufacturingOrdersHandler;
+
     @Override
     public GetCustomerOrderResponse getCustomerOrder(GetCustomerOrderQuery query) {
         CustomerOrder order = customerOrderRepositoryPort.findById(query.id());
+
+        List<GetManufacturingOrderResponse> manufacturingOrders = getCustomersManufacturingOrdersHandler.getManufacturingOrders(new GetCustomersManufacturingOrdersQuery(query.id()));
+
         return new GetCustomerOrderResponse(
                 order.getId(),
                 order.getCustomerId(),
-                order.getItems(),
+                manufacturingOrders.stream().map(CustomerOrderItemResponse::fromManufacturingOrderResponse).toList(),
                 order.getStatus(),
                 order.getInfo()
         );
